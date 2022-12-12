dbd1f7c0062b9ce2d32518dde5a53feb3302a89e
Sunil Khiatani <skhiatani@gmail.com>
2022-12-12 21:33:06 +0800

    modularized resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,28 +1,7 @@
-const _ = require('lodash')
+const queries = require('./resolvers/query');
+const sessions = require('./resolvers/session');
 
 module.exports = {
-    Query: {
-        sessions: (parent, args, {dataSources}, info) => {
-            return dataSources.sessionAPI.getSessions(args);
-        },
-        sessionById: (parent, {id}, {dataSources}, info) => {
-            return dataSources.sessionAPI.getSessionById(id);
-        },
-        speakers: (parent, args, {dataSources}, info) => {
-            return dataSources.speakerAPI.getSpeakers();
-        },
-        speakerById: (parent, {id}, {dataSources}, info) => {
-            return dataSources.speakerAPI.getSpeakerById(id);
-        }
-    },
-    Session: {
-        async speakers(session, args, {dataSources}) {
-            const speakers = await dataSources.speakerAPI.getSpeakers();
-            const returns = speakers.filter((speaker) => {
-                return _.filter(session.speakers, {id: speaker.id}).length > 0;
-            });
-
-            return returns;
-        }
-    }
+    Query: queries,
+    Session: sessions,
 }
